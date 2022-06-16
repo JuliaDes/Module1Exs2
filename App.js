0cc89c823d370b5ae28d802c08fe3131dcdab9a4
Julia D <desachy.yuliya@gmail.com>
2022-06-16 10:34:14 +0200

    React Native, Component View, Style

---
+++ App.js
@@ -0,0 +1,28 @@
+// import { StyleSheet, View } from 'react-native';
+
+const { View } = require("react-native-web");
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={{backgroundColor:"green",width:200,height:500}}></View>
+//       <View style={{backgroundColor:"red",width:200,height:500}}></View>
+//       <View style={{backgroundColor:"blue",width:100,height:500}}></View>
+//       <StatusBar style="auto" />
+//     </View>
+    
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
